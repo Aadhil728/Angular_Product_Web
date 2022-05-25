8c567b4465655ce9574126c0b987863a45ab800a
Aadhil <aadhil@devoloper.com>
2022-05-25 18:06:40 +0530

    initia;l commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+
+  constructor(){}
+
+
+  ngOnInit(){}
+
+  url1:string = "../assets/product1.jpg"
+  url2:string = "../assets/product2.jpg"
+  url3:string = "../assets/product3.jpg"
+  url4:string = "../assets/product4.jpg"
+
+
+
+  changeImage1(event:any){
+    this.url1 = event.target.src;
+  }
+
+    changeImage2(event:any){
+      this.url2 = event.target.src;
+    }
+
+    changeImage3(event:any){
+      this.url3 = event.target.src;
+    }
+
+    changeImage4(event:any){
+      this.url3 = event.target.src;
+    }
 }
